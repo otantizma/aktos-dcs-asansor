d5e198d67c57ee1b9e78a3ed4a715ead357c4790
can <can@debian.localhost>
2015-08-06 01:18:02 +0930

    second commit

--- controller.py
+++ controller.py
@@ -1,17 +1,23 @@
-from aktos_dcs import Actor,  ProxyActor, sleep, joinall
-from aktos_dcs import gevent_actor
-import gevent
-from aktos_dcs.Messages import *
-from gevent.socket import wait_read
-import sys
+from aktos_dcs import *
 
 class Controller(Actor):
     def __init__(self):
         Actor.__init__(self)
+        self.control()
 
-    def mesaj_gonder(self):
-        self.send(MotorMessage(direction="up",reciever="motor"))
+    def control(self):
+        while True:
+            print "will sen up"
+            self.send(MotorMessage(direction="up"))
+            sleep(3)
+            print "stop now"
+            self.send(MotorMessage(direction="stop"))
+            sleep(1)
+            print "down"
+            self.send(MotorMessage(direction="down"))
+            sleep(3)
 
 
 if __name__ == "__main__":
-    pass
+    ProxyActor()
+    joinall([Controller()])
--- motor.py
+++ motor.py
@@ -1,17 +1,4 @@
-from aktos_dcs import Actor,  ProxyActor, sleep, joinall
-from aktos_dcs import gevent_actor
-import gevent
-from aktos_dcs.Messages import *
-from gevent.socket import wait_read
-import sys
-
-class Cabin(Actor):
-    def __init__(self):
-        Actor.__init__(self)
-        self.height = 0
-
-    def handle_CabinMessage(self,msg):
-        self.height = msg.height
+from aktos_dcs import *
 
 class Motor(Actor):
     # 3 yon mesaji var
@@ -21,26 +8,31 @@ class Motor(Actor):
         Actor.__init__(self)
         self.direction = "stop"
         self.cabin_height = 0
+        self.move_cabin()
 
     def handle_MotorMessage(self,msg):
-        if msg.reciever == "motor":
-            if self.direction != msg.direction:
-                self.direction = msg.direction
-                kill(movekabin)
-                if msg.direction != "stop":
-                    self.direction = msg.direction
-                    spawn(movekabin)
+        self.direction = msg.direction
 
     def move_cabin(self):
-        if self.direction == "up":
-            self.cabin_height = self.cabin_height + 1
-        elif self.direction == "down":
-            self.cabin_height = self.cabin_height - 1
-        sleep(1)
-        self.send(CabinMessage(height=self.cabin_height))
-        if self.direction == "stop":
-            gevent.kill(self.move_cabin())
+        while True:
+            if self.direction == "up":
+                self.cabin_height = self.cabin_height + 1
+
+                self.send(ViewerMessage(height=self.cabin_height))
+
+            elif self.direction == "down":
+                self.cabin_height = self.cabin_height - 1
+                self.send(ViewerMessage(height=self.cabin_height))
+
+            sleep(1) # update interval
+
+class Viewer(Actor):
+    def handle_ViewerMessage(self,msg):
+        print time.time(), msg.height
 
 
 if __name__ == "__main__":
-    pass
+    ProxyActor()
+    view = Viewer()
+    motor = Motor()
+    joinall([view, motor])
